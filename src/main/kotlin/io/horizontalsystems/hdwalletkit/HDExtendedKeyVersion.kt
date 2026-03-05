8f873a179f66538812b6cc4b74b12ae9b883b9ad
Oleg Leonov <pomni.o.nas@gmail.com>
2026-03-05 17:05:37 +0700

    Add Dogecoin and Dash support to HDExtendedKeyVersion

--- src/main/kotlin/io/horizontalsystems/hdwalletkit/HDExtendedKeyVersion.kt
+++ src/main/kotlin/io/horizontalsystems/hdwalletkit/HDExtendedKeyVersion.kt
@@ -27,7 +27,15 @@ enum class HDExtendedKeyVersion(
 
     // litecoin bip49
     Mtpv(0x01b26792, "Mtpv"),
-    Mtub(0x01b26ef6, "Mtub");
+    Mtub(0x01b26ef6, "Mtub"),
+
+    // dogecoin bip44
+    dgpv(0x02fac398, "dgpv"),
+    dgub(0x02facafd, "dgub"),
+
+    // dash bip44
+    drkv(0x02fe52cc, "drkv"),
+    drkp(0x02fe52f8, "drkp");
 
     val coinTypes: List<ExtendedKeyCoinType>
         get() = when (this) {
@@ -42,6 +50,14 @@ enum class HDExtendedKeyVersion(
             Ltpv, Ltub, Mtpv, Mtub -> {
                 listOf(ExtendedKeyCoinType.Litecoin)
             }
+
+            dgpv, dgub -> {
+                listOf(ExtendedKeyCoinType.Dogecoin)
+            }
+
+            drkv, drkp -> {
+                listOf(ExtendedKeyCoinType.Dash)
+            }
         }
 
     val purposes: List<Purpose>
@@ -50,7 +66,7 @@ enum class HDExtendedKeyVersion(
                 listOf(Purpose.BIP44, Purpose.BIP86)
             }
 
-            Ltpv, Ltub -> {
+            Ltpv, Ltub, dgpv, dgub, drkv, drkp -> {
                 listOf(Purpose.BIP44)
             }
 
@@ -70,19 +86,21 @@ enum class HDExtendedKeyVersion(
             zprv -> zpub
             Ltpv -> Ltub
             Mtpv -> Mtub
-            xpub, ypub, zpub, Ltub, Mtub -> this
+            dgpv -> dgub
+            drkv -> drkp
+            xpub, ypub, zpub, Ltub, Mtub, dgub, drkp -> this
         }
 
     val privKey: HDExtendedKeyVersion
         get() = when (this) {
-            xprv, yprv, zprv, Ltpv, Mtpv -> this
-            xpub, ypub, zpub, Ltub, Mtub -> throw IllegalStateException("No privateKey of $base58Prefix")
+            xprv, yprv, zprv, Ltpv, Mtpv, dgpv, drkv -> this
+            xpub, ypub, zpub, Ltub, Mtub, dgub, drkp -> throw IllegalStateException("No privateKey of $base58Prefix")
         }
 
     val isPublic: Boolean
         get() = when (this) {
-            xprv, yprv, zprv, Ltpv, Mtpv -> false
-            xpub, ypub, zpub, Ltub, Mtub -> true
+            xprv, yprv, zprv, Ltpv, Mtpv, dgpv, drkv -> false
+            xpub, ypub, zpub, Ltub, Mtub, dgub, drkp -> true
         }
 
     companion object {
@@ -96,6 +114,8 @@ enum class HDExtendedKeyVersion(
                     when (coinType) {
                         ExtendedKeyCoinType.Bitcoin -> if (isPrivate) xprv else xpub
                         ExtendedKeyCoinType.Litecoin -> if (isPrivate) Ltpv else Ltub
+                        ExtendedKeyCoinType.Dogecoin -> if (isPrivate) dgpv else dgub
+                        ExtendedKeyCoinType.Dash -> if (isPrivate) drkv else drkp
                     }
                 }
 
@@ -103,6 +123,8 @@ enum class HDExtendedKeyVersion(
                     when (coinType) {
                         ExtendedKeyCoinType.Bitcoin -> if (isPrivate) yprv else ypub
                         ExtendedKeyCoinType.Litecoin -> if (isPrivate) Mtpv else Mtub
+                        ExtendedKeyCoinType.Dogecoin,
+                        ExtendedKeyCoinType.Dash -> throw IllegalStateException("BIP49 not supported for $coinType")
                     }
                 }
 
@@ -125,5 +147,5 @@ enum class HDExtendedKeyVersion(
 }
 
 enum class ExtendedKeyCoinType {
-    Bitcoin, Litecoin
+    Bitcoin, Litecoin, Dogecoin, Dash
 }
